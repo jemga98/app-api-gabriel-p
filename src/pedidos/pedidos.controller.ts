57b58dd1d24f63f71ddefa235971ec3f5ed70c9e
jemga98 <123613519+jemga98@users.noreply.github.com>
2024-07-21 16:09:20 -0600

    Actualizacion de la documentacion de API en Swagger

--- src/pedidos/pedidos.controller.ts
+++ src/pedidos/pedidos.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
 import { PedidosService } from './pedidos.service';
 import { Pedido } from './pedido.entity';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 @Controller('pedidos')
 @ApiTags("Pedidos")
@@ -9,26 +9,31 @@ export class PedidosController {
   constructor(private readonly pedidosService: PedidosService) {}
 
   @Get()
+  @ApiOperation({ description: 'Visualizar todos los pedidos' })
   findAll(): Promise<Pedido[]> {
     return this.pedidosService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ description: 'Visualizar pedido especifico, indicando el Id' })
   findOne(@Param('id') id: string): Promise<Pedido> {
     return this.pedidosService.findOne(+id);
   }
 
   @Post()
+  @ApiOperation({ description: 'Agregar un nuevo pedido' })
   create(@Body() pedido: Pedido): Promise<Pedido> {
     return this.pedidosService.create(pedido);
   }
 
   @Put(':id')
+  @ApiOperation({ description: 'Editar todos los registro del pedido especifico, indicando el Id' })
   update(@Param('id') id: string, @Body() pedido: Partial<Pedido>): Promise<Pedido> {
     return this.pedidosService.update(+id, pedido);
   }
 
   @Delete(':id')
+  @ApiOperation({ description: 'Eliminar pedido especifico, indicando el Id' })
   remove(@Param('id') id: string): Promise<void> {
     return this.pedidosService.remove(+id);
   }
--- src/productos/productos.controller.ts
+++ src/productos/productos.controller.ts
@@ -3,7 +3,7 @@
 import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
 import { ProductosService } from './productos.service';
 import { Producto } from './producto.entity';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 @Controller('productos')
 @ApiTags("Productos")
@@ -11,26 +11,31 @@ export class ProductosController {
   constructor(private readonly productosService: ProductosService) {}
 
   @Get()
+  @ApiOperation({ description: 'Obtener todos los productos' })
   findAll(): Promise<Producto[]> {
     return this.productosService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ description: 'Obtener producto especifico, indicando el Id' })
   findOne(@Param('id') id: string): Promise<Producto> {
     return this.productosService.findOne(+id);
   }
 
   @Post()
+  @ApiOperation({ description: 'Crear un nuevo registro de producto' })
   create(@Body() producto: Producto): Promise<Producto> {
     return this.productosService.create(producto);
   }
 
   @Put(':id')
+  @ApiOperation({ description: 'Editar todos los datos del producto, indicando el Id' })
   update(@Param('id') id: string, @Body() producto: Partial<Producto>): Promise<Producto> {
     return this.productosService.update(+id, producto);
   }
 
   @Delete(':id')
+  @ApiOperation({ description: 'Eliminar un producto especifo, indicando el Id' })
   remove(@Param('id') id: string): Promise<void> {
     return this.productosService.remove(+id);
   }
